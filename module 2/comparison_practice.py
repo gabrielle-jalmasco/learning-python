890e53316e9f71c1f06712806d7ff39b0fdf2e12
Gab <jalmascogab002@gmail.com>
2026-02-01 22:01:57 +0800

    update new module.

---
+++ module 2/comparison_practice.py
@@ -0,0 +1,21 @@
+user_age = int(input("Enter age: "))
+
+print(f"Age: {user_age}")
+print(f"Are you a teenager (13-19)? Age >= 13: {user_age >= 13}")
+print(f"Are you a adult (18+)? Age >= 18: {user_age >= 18}")
+print(f"Are you a senior (65+)? Age >= 65: {user_age >= 65}")
+print(f"Are you exactly 25? Age == 25: {user_age == 25}\n")
+
+item_price = float(input("Enter price: $"))
+budget = float(input("Enter budget: $"))
+
+print(f"\nCan you afford it? {item_price <= budget}")
+print(f"Is it over budget? {item_price > budget}")
+print(f"Is it exactly your budget? {item_price == budget}")
+print(f"Do you have money left? {budget > item_price}\n")
+
+correct_password = "Python123"
+user_password = input("Enter password: ")
+
+print(f"\nPassword correct? {user_password == correct_password}")
+print(f"Password incorrect? {user_password != correct_password}")
--- module 2/comparisons.py
+++ module 2/comparisons.py
@@ -2,3 +2,31 @@ age = 25
 temperature = 72
 price = 19.99
 username = "Gab"
+
+# ==
+print(f"Is age equal to 25? {age == 25}")
+print(f"Is age equal to 30? {age == 30}")
+
+# !=
+print(f"\nIs temperature not 70? {temperature != 70}")
+print(f"\nIs username not 'Alex'? {username != 'Alex'}")
+
+# >
+print(f"\nIs age greater than 18? {age > 18}")
+print(f"Is age greater than 30? {age > 30}")
+
+# <
+print(f"\nIs price less than 20? {price < 20}")
+print(f"Is price less than 10? {price < 10}")
+
+# >=
+print(f"\nIs age >= 25? {age >= 25}")
+print(f"Is age >= 26? {age >= 26}")
+
+# <=
+print(f"\nIs temperature <= 72? {temperature <= 72}")
+print(f"Is temperature <= 71? {temperature <= 71}")
+
+# == vs. =
+print("\n= is for ASSIGNMENT (setting a value)")
+print("== is for COMPARISON (checking if equal)")
